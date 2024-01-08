c7e1e62111ef044c33b5ec8875f640a76c001f5c
Aventel <matblairon@gmail.com>
2024-01-08 15:08:57 +0100

    Test: add test for UpdateBook

--- springboot-rest-demo-ws/src/test/java/org/grostarin/springboot/demorest/services/BookServiceTest.java
+++ springboot-rest-demo-ws/src/test/java/org/grostarin/springboot/demorest/services/BookServiceTest.java
@@ -4,6 +4,7 @@ import org.grostarin.springboot.demorest.domain.BannedBook;
 import org.grostarin.springboot.demorest.domain.Book;
 import org.grostarin.springboot.demorest.exceptions.BookBannedException;
 import org.grostarin.springboot.demorest.exceptions.BookIdMismatchException;
+import org.grostarin.springboot.demorest.exceptions.BookNotFoundException;
 import org.grostarin.springboot.demorest.repositories.BannedBookRepository;
 import org.grostarin.springboot.demorest.repositories.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -50,6 +52,23 @@ class BookServiceTest {
         Book toCreate = new Book("title", "author");
         toCreate.setId(1L);
         bookService.create(toCreate);
-        assertThrows(BookIdMismatchException.class, (() -> bookService.updateBook(toCreate, 2L)));
+        assertThrows(BookIdMismatchException.class, (() -> bookService.updateBook(toCreate, 1L)));
+    }
+
+    @Test
+    public void testUpdateBookNotInRepo() {
+        Book toCreate = new Book("title", "author");
+        toCreate.setId(1L);
+        assertThrows(BookNotFoundException.class, (() -> bookService.updateBook(toCreate, 1L)));
+    }
+
+    @Test
+    public void testUpdateBook() {
+        Book toCreate = new Book("title", "author");
+        toCreate.setId(1L);
+        bookService.create(toCreate);
+        toCreate.setTitle("new title");
+        bookService.updateBook(toCreate, 1L);
+        assertEquals(toCreate.getTitle(), bookService.findOne(1L).getTitle());
     }
 }
